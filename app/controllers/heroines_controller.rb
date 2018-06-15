70e0df650ad1521f43f7e3b681418992779503d5
mrstevenaweiss <weiss.steven@gmail.com>
2018-06-15 11:57:47 -0400

    Done

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,35 @@
 class HeroinesController < ApplicationController
+
   def index
-    @heroines = Heroine.all
+    if params[:power]
+      @heroine = Heroine.where('power LIKE ?', "%#{params[:power]}%")
+    else
+      @heroines = Heroine.all
+    end
+  end
+
+  def new
+    @heroine = Heroine.new
+    render :new
+  end
+
+  def create
+    @heroine = Heroine.create(heroine_params)
+
+    redirect_to heroine_path(@heroine)
+  end
+
+  def show
+    @heroine = find_heroine
+    render :show
+  end
+
+  private
+  def find_heroine
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:power, :name, :super_name, :power_id)
   end
 end
