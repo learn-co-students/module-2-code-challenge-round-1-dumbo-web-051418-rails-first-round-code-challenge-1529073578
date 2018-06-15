3aa2612299530acbd31c5f9170a4f0296139421d
Razibul Ahmed <razibul.ahmed@gmail.com>
2018-06-15 11:58:55 -0400

    Mod_2 Code challenge worked on

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,36 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def new
+    @heroine = Heroine.new
+    render :new
+  end
+
+  def create
+
+    @heroine = Heroine.create(heroine_params)
+    if @heroine.valid?
+      redirect_to @heroine
+    else
+      render :new
+    end
+  end
+
+
+  def show
+    @heroine = Heroine.find(params[:id])
+    render :show
+  end
+
+  def search
+    @projects = Project.search(params[:search])
+    render :index
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
 end
