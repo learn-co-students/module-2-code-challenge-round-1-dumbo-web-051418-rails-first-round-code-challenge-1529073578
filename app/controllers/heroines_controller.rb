ffdd6daefa6a7a9529374f28edde446c5f43eca5
github username <github email address>
2018-06-15 11:56:43 -0400

    done

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,28 @@
 class HeroinesController < ApplicationController
   def index
-    @heroines = Heroine.all
+    @heroines = nil
+    if params[:filter_by_power] == nil
+      @heroines = Heroine.all
+    elsif Power.find_by(name: params[:filter_by_power]) != nil
+      @heroines = Heroine.all.select { |heroine| heroine.power_id == Power.find_by(name: params[:filter_by_power]).id}
+    end
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.create(heroine_params)
+    redirect_to heroine_path(@heroine)
+  end
+
+  private
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
   end
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,11 @@
+# Heroine may have only one power
+
+
+# HEROINE >--- POWER
+
+
 class Heroine < ApplicationRecord
+  belongs_to :power
+
+  validates :super_name, uniqueness: true
 end
