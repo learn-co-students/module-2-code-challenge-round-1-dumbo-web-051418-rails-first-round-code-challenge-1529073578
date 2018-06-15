ec24aaf14fb0025daf8a0da944be9b0b6ab64d77
prmoreira23 <prm.gredes@gmail.com>
2018-06-15 11:42:28 -0400

    Done.

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,36 @@
+require 'pry'
+
 class HeroinesController < ApplicationController
   def index
-    @heroines = Heroine.all
+
+    if params[:power] && params[:power] != ""
+      power = Power.find_by(name: params[:power])
+      @heroines = power.heroines
+    else
+      @heroines = Heroine.all
+    end
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+
+    if @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      render :new
+    end
+  end
+
+  private
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
   end
 end
