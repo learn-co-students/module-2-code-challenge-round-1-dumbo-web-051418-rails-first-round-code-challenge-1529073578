644bb24fee684b640f8f85388ad790913f898e03
github username <github email address>
2018-06-15 11:59:01 -0400

    code challenge completion

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,27 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def create
+    heroine_params
+    byebug
+
+    heroine = Heroine.create(heroine_params)
+    redirect_to heroine_path(heroine)
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  private
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
 end
