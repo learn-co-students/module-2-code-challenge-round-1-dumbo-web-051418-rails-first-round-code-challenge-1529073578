950c06c0f91358b3638f2d56def5e70c169a9865
pablopawpaw <lpault29@gmail.com>
2018-06-15 17:09:19 -0400

    removed unnecessary route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   resources :heroines, only: [:index, :show, :new, :create] do
     collection do
       get :search, :action => 'search_post', :as => 'search_post'
-      get 'search/:q', :action => 'search', :as => 'search'
     end
   end
 
