c83d4c4efa570b7384e33fbb0a6b63ff35f5e30b
github username <github email address>
2018-06-15 11:58:51 -0400

    Hi, I didn't finish. Please be nice :D

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,15 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def show
+    @power = Power.find(params[:id])
+    render :show
+  end
+
+  private
+
+  def power_params
+    params.require(:power).permit(:name)
+  end
 end
