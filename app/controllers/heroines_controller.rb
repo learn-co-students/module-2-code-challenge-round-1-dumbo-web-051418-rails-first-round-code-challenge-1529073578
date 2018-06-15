e1991f84f3fe6b4ee7c76bd6d48ce183813a4d77
github username <github email address>
2018-06-15 11:59:30 -0400

    Mod 2 Challenge

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,29 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.find_or_create_by(name: heroine_params[:name], super_name: heroine_params[:super_name])
+    @power = Power.find_by(name: heroine_params[:power])#finds the power object given the name of power
+
+    @heroine.power = @power #associate heroine with the power
+    @heroine.save
+
+    redirect_to heroine_path(@heroine)
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  private
+  def heroine_params
+  params.require(:heroine).permit(:name, :super_name, :power)
+  end
+
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,6 @@
 class Heroine < ApplicationRecord
+  belongs_to :power
+
+
+
 end
